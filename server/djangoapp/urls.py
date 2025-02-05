b774586f84b9f657e43eae23253c9e94fd839737
sidselothcodes <sidselothcodes@gmail.com>
2025-02-05 17:16:43 -0500

    Added carmake and car model

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
 
     # path for add a review view
 
+    # path for get cars view
+    path('get_cars', views.get_cars, name='get_cars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
