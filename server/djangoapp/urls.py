1f7a9430e72803e7fef2359d9d5f8eb67277d6b5
sidselothcodes <sidselothcodes@gmail.com>
2025-02-10 15:57:07 -0500

    Converted JSON results into Python objects

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from . import views
 
 
+
+
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
@@ -18,10 +20,16 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for get cars view
     path('get_cars', views.get_cars, name='get_cars'),
+    path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
